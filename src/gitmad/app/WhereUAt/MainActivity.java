a00bb36c63ce2d0bef1e4a2c36a0e46fd9bcc2d9
David Greenhalgh <david@david-desktop.(none)>
2013-02-10 14:04:58 -0500

    Added ap-name, button_find_me, edit_ZIP, and welcome to strings.xml. Added ZIP EditText

--- src/gitmad/app/WhereUAt/MainActivity.java
+++ src/gitmad/app/WhereUAt/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.EditText;
 
 public class MainActivity extends Activity implements OnClickListener {
     /** Called when the activity is first created. */
