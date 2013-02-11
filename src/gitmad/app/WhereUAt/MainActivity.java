ea70030635c4fedd0987fa8ea029d4d69db783cf
David Greenhalgh <david@david-desktop.(none)>
2013-02-11 00:05:23 -0500

    Added Geocoder locality functionality and linked EditText in MainActivity to Result Activity

--- src/gitmad/app/WhereUAt/MainActivity.java
+++ src/gitmad/app/WhereUAt/MainActivity.java
@@ -9,6 +9,9 @@ import android.view.View.OnClickListener;
 import android.widget.EditText;
 
 public class MainActivity extends Activity implements OnClickListener {
+	
+	public final static String EXTRA_MESSAGE = "gitmad.app.WhereUAt";
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -21,6 +24,9 @@ public class MainActivity extends Activity implements OnClickListener {
     @Override
 	public void onClick(View v) {
 		Intent i = new Intent(this, ResultActivity.class); //private intent
+		EditText editText = (EditText)findViewById(R.id.editText1);
+		String message = editText.getText().toString();
+		i.putExtra(EXTRA_MESSAGE, message);
 		startActivity(i);
 		Log.d("Demo", "Find me button pressed");		
 	}
--- src/gitmad/app/WhereUAt/ResultActivity.java
+++ src/gitmad/app/WhereUAt/ResultActivity.java
@@ -1,11 +1,17 @@
 package gitmad.app.WhereUAt;
 
+import java.io.IOException;
+import java.util.List;
+
 import android.app.Activity;
+import android.content.Intent;
+import android.location.*;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class ResultActivity extends Activity implements OnClickListener {
 	private String result = "not set";
@@ -17,10 +23,32 @@ public class ResultActivity extends Activity implements OnClickListener {
 		setContentView(R.layout.result);
 		Log.d("Demo", "Atlanta, GA : the location");
 		TextView resultView = (TextView) this.findViewById(R.id.result_value_label);
-		resultView.setText("Atlanta, GA");
 
 		View returnButton = findViewById(R.id.back_to_menu_button);
 		returnButton.setOnClickListener(this);
+		
+		final Geocoder geocoder = new Geocoder(this);
+		
+		Intent i = getIntent();
+		String ZIP = i.getStringExtra(MainActivity.EXTRA_MESSAGE);
+		try
+		{
+			List<Address> addresses = geocoder.getFromLocationName(ZIP, 1);
+			if(addresses != null && !addresses.isEmpty())
+			{
+				Address address = addresses.get(0);
+				String message = String.format("%s", address.getLocality());
+				resultView.setText(message);
+			}
+			else
+			{
+				resultView.setText("Unable to locate ZIP");
+			}
+		}
+		catch (IOException e)
+		{
+			
+		}
 	}
 	
 	@Override
